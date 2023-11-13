72b6c18d913542161b93e886f4df08cb5a22cc59
huynh13 <80864294+huynh13@users.noreply.github.com>
2023-11-13 11:13:41 +0700

    Code slide 3

--- app/src/main/java/vn/poly/mob305/slide1_java/MainActivity.java
+++ app/src/main/java/vn/poly/mob305/slide1_java/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,20 +15,129 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import vn.poly.mob305.slide1_java.json.Info;
+import vn.poly.mob305.slide1_java.json.Result;
 import vn.poly.mob305.slide1_java.json.Root;
+import vn.poly.mob305.slide1_java.retrofit.RandomService;
 
 public class MainActivity extends AppCompatActivity {
 
     String api = "https://jsonplaceholder.typicode.com/todos/1";
+    // https://jsonplaceholder.typicode.com : base_url
+    // todos : path : địa chỉ - đường dẫn
+    // 1 : path
+
     String apiPost = "https://jsonplaceholder.typicode.com/posts";
+    // base_url = "https://jsonplaceholder.typicode.com"
+    // posts : path
 
     String randomUser = "https://api.randomuser.me/?results=10";
+
+    // BASE_URL : https://api.randomuser.me/
+    // Params : results = 10 : query param !!!
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GetAsyncTask asyncTask = new GetAsyncTask();
+        findViewById(R.id.btnRequest).setOnClickListener(v ->{
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://api.randomuser.me/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            RandomService randomService = retrofit.create(RandomService.class);
+            Call<Root> userList = randomService.getListUser(10);
+            userList.enqueue(new Callback<Root>() {
+                @Override
+                public void onResponse(Call<Root> call, Response<Root> response) {
+                    Root root = response.body();
+                    Info info = root.getInfo();
+                    List<Result> userList = root.getResults();
+
+                }
+
+                @Override
+                public void onFailure(Call<Root> call, Throwable t) {
+                    Toast.makeText(MainActivity.this,
+                            t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+
+        });
+        findViewById(R.id.btnReq2).setOnClickListener(v->{
+           /* String api = "https://jsonplaceholder.typicode.com/todos/1";
+            // https://jsonplaceholder.typicode.com : base_url
+            // todos : path : địa chỉ - đường dẫn
+            // 1 : path*/
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://jsonplaceholder.typicode.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            RandomService randomService = retrofit.create(RandomService.class);
+
+            Call<ResponseBody> toDO = randomService.getToDo(10);
+            toDO.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    String ketqua = null;
+                    try {
+                        ketqua = response.body().string();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Log.e("kq",ketqua);
+                }
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    Log.e("FAIL",t.getMessage());
+                }
+            });
+
+
+        });
+        findViewById(R.id.btnPost).setOnClickListener(v->{
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://jsonplaceholder.typicode.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            RandomService randomService = retrofit.create(RandomService.class);
+
+            Call<ResponseBody> userR = randomService.sendUser(10,"BODY",
+                    "TITLEEE");
+            userR.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    try {
+                        Log.e("Data",response.body().string());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    Log.e("LOI",t.getMessage());
+                }
+            });
+
+
+        });
+    /*    GetAsyncTask asyncTask = new GetAsyncTask();
         asyncTask.setMyListener(new MyListener() {
             @Override
             public void onUpdate(int progress) {
@@ -41,7 +151,6 @@ public class MainActivity extends AppCompatActivity {
                 Root rootM = new Gson().fromJson(result,Root.class);
                 Toast.makeText(MainActivity.this, rootM.getInfo().getVersion() + "",
                         Toast.LENGTH_SHORT).show();
-
 
                 try {
                     JSONObject root = new JSONObject(result);
@@ -70,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Starting...", Toast.LENGTH_SHORT).show();
             }
         });
-        asyncTask.execute(randomUser,"GET");
+        asyncTask.execute(randomUser,"GET");*/
     }
 
 }
---
+++ app/src/main/java/vn/poly/mob305/slide1_java/retrofit/RandomService.java
@@ -0,0 +1,37 @@
+package vn.poly.mob305.slide1_java.retrofit;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import vn.poly.mob305.slide1_java.json.Root;
+
+public interface RandomService {
+    // dinh nghia request tai day !!!!
+
+
+    //String randomUser = "https://api.randomuser.me/?results=10";
+    // BASE_URL : https://api.randomuser.me/
+    // Params : results = 10 : query param !!!
+
+    // http request GET tới base URL, có tham số là results
+    // trong <> sau Call sẽ là class bóc json bằng GSON !!!!
+    @GET("/")
+    Call<Root> getListUser(@Query("results") int results);
+
+    @GET("/todos/{id}")
+    Call<ResponseBody> getToDo(@Path("id") int id);
+
+    @POST("/posts")
+    @FormUrlEncoded // request được encode khi gửi đi
+    Call<ResponseBody> sendUser(@Field("userId") int userId,
+                                @Field("body") String body,
+                                @Field("title") String title);
+
+
+
+}
